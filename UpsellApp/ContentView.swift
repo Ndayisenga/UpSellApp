ca072a73019e285119181914280bcacaa73249ad
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-17 22:52:16 +0200

    Download My APPfrom appStore

--- UpsellApp/ContentView.swift
+++ UpsellApp/ContentView.swift
@@ -4,13 +4,42 @@
 //
 //  Created by Ndayisenga Jean Claude on 17/05/2021.
 //
-
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
+    @State var appStoreOverlayPresented: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button(action: {
+                    appStoreOverlayPresented.toggle()
+                    
+                }, label: {
+                    Label(
+                        title: { Text("Download")
+                            .foregroundColor(Color.white)
+                            
+                        },
+                        icon: {
+                            Image("Instagram")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 55, height: 55)
+                                .cornerRadius(8)
+                        }
+                    )
+                    .frame(width: 200)
+                    .padding()
+                    .background(Color.purple)
+                    .clipShape(Capsule())
+                })
+                .appStoreOverlay(isPresented: $appStoreOverlayPresented) {
+                    SKOverlay.AppConfiguration(appIdentifier: "123123123", position: .bottomRaised)
+                }
+                    
+            }
+        }
     }
 }
 
