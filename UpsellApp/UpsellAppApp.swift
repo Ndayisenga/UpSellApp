1af22ba6be4c74cfc5ecb3a6f59c3b1569b522b3
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-17 22:23:49 +0200

    Initial Commit

---
+++ UpsellApp/UpsellAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpsellAppApp.swift
+//  UpsellApp
+//
+//  Created by Ndayisenga Jean Claude on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct UpsellAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
